caef0bd86f9ccb7a853fc3e4cd312e618de133d9
J0nteR <jonathan.ronnas@gmail.com>
2025-02-18 09:46:21 +0100

    första utkast och alla vyer skapade

--- app.rb
+++ app.rb
@@ -2,4 +2,29 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+# require 'becrypt'
+
+enable :sessions
+
+get('/') do
+  slim :home
+end
+
+get('/saved') do
+  slim :saved
+end
+
+get('/login') do
+  slim :login
+end
+
+get('/clear_session') do
+  session.clear
+  slim(:home)
+end
+
+post('/login') do
+  session[:name] = params[:username]
+  session[:password] = params[:password]
+  redirect('/login')
+end
