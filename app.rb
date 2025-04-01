1ffe05824f286db08aaaf15e4b16a956865233d6
J0nteR <jonathan.ronnas@gmail.com>
2025-04-01 09:43:10 +0200

    v14

--- app.rb
+++ app.rb
@@ -7,14 +7,25 @@ require 'sinatra/reloader'
 enable :sessions
 
 get('/') do
-  slim :home
-  end
-
-get('/saved') do
   db = SQLite3::Database.new("db/project2025.db")
-  db.results_as_hash = true #Få svar i strukturen [{},{},{}]
+  db.results_as_hash = true
 
   @data = db.execute("SELECT * FROM recipes")
+  slim :home
+end
+
+get('/recipe/:nummer') do
+  @receptid = params[:nummer].to_i
+  db = SQLite3::Database.new("db/project2025.db")
+  db.results_as_hash = true
+
+  @data2 = db.execute("SELECT * FROM recipes WHERE id = #{@receptid}")
+
+  slim :saved
+end
+
+get('/saved') do
+
   slim :saved
 end
 
